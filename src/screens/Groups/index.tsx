eb3f4ab8ea5fcad73be241b3c1a145beb2e9636a
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-08-05 12:42:45 -0300

    inital commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+import { Header } from '@components/Header';
+
+export  function Groups() {
+  return (
+    <Container>
+      <Header  />
+    </Container>
+  );
+}
+
