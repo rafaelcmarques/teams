f84eb1128e44d8a8c7685d64a2bf21c8adf9f7f8
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-08-09 21:21:19 -0300

    criado componente Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,13 @@
+import { TextInputProps } from "react-native";
+import { Container } from "./styled";
+import { useTheme } from "styled-components/native";
+
+export function Input({...rest}:TextInputProps){
+  const {COLORS} = useTheme()
+  return(
+    <Container
+      placeholderTextColor={COLORS.GRAY_300}
+      {...rest}
+    />
+  )
+}
